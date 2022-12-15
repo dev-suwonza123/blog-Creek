f63fac744b9fb57a47a5704a3bf8e51718d1933e
mamvav <loopperz19@hotmail.com>
2022-12-15 13:39:48 +0700

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -6,7 +6,7 @@ const posts = Object.values(postImportResult);
 export const get = () =>
   rss({
     title: "NAZA เว็บตรงปลอดภัย",
-    description: "A Theme for Astro",
+    description: "NAZAVIP",
     site: import.meta.env.SITE,
     items: import.meta.glob("./posts/**/*.md"),
   });
